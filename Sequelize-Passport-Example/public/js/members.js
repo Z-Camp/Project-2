144d33b8ae3e5bbb343046d16710b067d1e16d86
alecweems <a.weems721@gmail.com>
2020-05-18 19:59:18 -0400

    added hide/show toggle for buttons

--- Sequelize-Passport-Example/public/js/members.js
+++ Sequelize-Passport-Example/public/js/members.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
   });
 });
+
+function progressToggle() {
+  let x = document.getElementById("progress-entries")
+  if (x.style === "hidden") {
+    x.style = "none";
+  } else {
+    x.style = "show";
+  }
+};
