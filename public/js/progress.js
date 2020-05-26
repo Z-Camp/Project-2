309d5500ac7b74cad2f6444d00c0baf491d10307
Romo <60673021+Z-Camp@users.noreply.github.com>
2020-05-25 21:49:10 -0400

    styling:

--- public/js/progress.js
+++ public/js/progress.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
  $.get("/api/user_data").then(function (data) {
         let userId = data.id;
         $.get("/api/activities/" + userId).then(function (data) {
-        console.log(data);
         for (let i = 0; i < data.length; i++) {
           $("#tableBody").append(`
           <tr>
@@ -10,7 +9,7 @@ $(document).ready(function() {
             <td>${data[i].activityType}</td>
             <td>${data[i].distance}</td>
             <td>${data[i].time}</td>
-            <td>${JSON.stringify((data[i].dateTime).split("T")[0])}</td>
+            <td>${(data[i].dateTime).split("T")[0]}</td>
           </tr>
           `)
         }
